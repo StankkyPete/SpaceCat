a33606f01158edf8c71a09b8c49861213a571b14
StankkyPete <Peter@Peters-MacBook-Air.local>
2015-01-10 15:51:44 -0500

    Initial Commit

---
+++ SpaceCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpaceCat
+//
+//  Created by Peter Dorsaneo on 1/10/15.
+//  Copyright (c) 2015 Peter Dorsaneo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
